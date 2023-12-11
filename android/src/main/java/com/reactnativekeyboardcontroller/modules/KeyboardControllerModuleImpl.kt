1854674d09418b856593645924122673b3a2d277
Mathieu Acthernoene <zoontek@gmail.com>
2023-12-11 13:54:38 +0100

    Add KeyboardController.init

--- android/src/main/java/com/reactnativekeyboardcontroller/modules/KeyboardControllerModuleImpl.kt
+++ android/src/main/java/com/reactnativekeyboardcontroller/modules/KeyboardControllerModuleImpl.kt
@@ -2,8 +2,13 @@ package com.reactnativekeyboardcontroller.modules
 
 import android.content.Context
 import android.view.View
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.UiThreadUtil
 import com.reactnativekeyboardcontroller.traversal.FocusedInputHolder
@@ -62,5 +67,43 @@ class KeyboardControllerModuleImpl(private val mReactContext: ReactApplicationCo
 
   companion object {
     const val NAME = "KeyboardController"
+
+    fun init(activity: Activity, styles: String) {
+      val haveDarkContent = "dark-content" == styles
+      val window = activity.window
+      val insetsController = WindowInsetsControllerCompat(window, window.decorView)
+
+      WindowCompat.setDecorFitsSystemWindows(window, false)
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+        window.clearFlags(
+          WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+        )
+
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+
+        insetsController.isAppearanceLightStatusBars = haveDarkContent
+        insetsController.isAppearanceLightNavigationBars = haveDarkContent
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+          window.isStatusBarContrastEnforced = false
+          window.isNavigationBarContrastEnforced = false
+        }
+      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+
+        window.statusBarColor = Color.TRANSPARENT
+
+        insetsController.isAppearanceLightStatusBars = haveDarkContent
+      } else {
+        window.addFlags(
+          WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+        )
+      }
+    }
   }
 }
